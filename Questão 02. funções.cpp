5a6971b9f211b23ce20f575632e2b35ab7597056
Renato <167001503+renat0ben@users.noreply.github.com>
2024-04-14 20:55:59 -0300

    Questão 02 funções em C
    OBJETIVO: Criar uma função, sem retorno, que recebe um valor inteiro por parâmetro e informa 
    se ele é positivo, negativo ou neutro.

---
+++ Questão 02. funções.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<locale.h>
+ 
+ void verifi (int n) {
+ 	if(n==0){
+ 		printf("%i é neutro", n);
+	 }
+	 else if(n<0){
+	 	printf("%i é negativo", n);
+	 }
+	 else{
+	 	printf("%i é positivo", n);
+	 }
+}
+int main(){
+	setlocale(LC_ALL, "Portuguese");
+	int n;
+	printf("Digite um número: \n");
+	scanf("%i", &n);
+	verifi(n);
+	return(0);
+}
+	 
